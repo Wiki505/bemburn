0c290fc2100dd75e08dc6ca1efe33673ed3f3001
LordPython <46406683+LordPython@users.noreply.github.com>
2019-12-03 14:41:27 -0600

    Add files via upload

---
+++ static/js/data-table.js
@@ -0,0 +1,45 @@
+//[Data Table Javascript]
+
+//Project:	Crypto Admin - Responsive Admin Template
+//Primary use:   Used only for the Data Table
+
+$(function () {
+    "use strict";
+
+    $('#datavulns1').DataTable();
+    $('#datavulns2').DataTable({
+      'paging'      : true,
+      'lengthChange': false,
+      'searching'   : false,
+      'ordering'    : true,
+      'info'        : true,
+      'autoWidth'   : false
+    });
+	
+	
+	$('#datavulns').DataTable( {
+		dom: 'Bfrtip',
+		buttons: [
+			'copy', 'csv', 'excel', 'pdf', 'print'
+		]
+	} );
+	
+	$('#tickets').DataTable({
+	  'paging'      : true,
+	  'lengthChange': false,
+	  'searching'   : false,
+	  'ordering'    : true,
+	  'info'        : true,
+	  'autoWidth'   : false,
+	});
+	
+	$('#employeelist').DataTable({
+	  'paging'      : true,
+	  'lengthChange': false,
+	  'searching'   : true,
+	  'ordering'    : true,
+	  'info'        : true,
+	  'autoWidth'   : false,
+	});
+	
+  }); // End of use strict
